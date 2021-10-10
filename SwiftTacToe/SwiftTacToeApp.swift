26c44ece06f39dfb7c3446dfc35c9a7635603466
Shaun Hubbard <shaunhubbard2013@icloud.com>
2021-10-10 12:48:46 -0400

    Initial Commit

---
+++ SwiftTacToe/SwiftTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTacToeApp.swift
+//  SwiftTacToe
+//
+//  Created by shaun on 10/10/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
